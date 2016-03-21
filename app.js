8244de1381c23e097346b557eb5a8c711188d481
Chamnap Chhorn <chamnapchhorn@gmail.com>
2016-03-21 16:26:03 +0700

    chore(): validate source file

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var path    = require('path');
 var prn2xls = require('./index.js');
 
 program
   .version('0.1.0')
-  .option('-p, --path [value]',             'full path of prn file')
+  .option('-p, --path [value]',             'full path of prn file, eg: PI1105.PRN')
   .option('-v, --verbose [value]',          'verbose mode')
   .parse(process.argv);
+
+var extName = path.extname(program.path);
+if (extName != '.PRN') {
+  throw new Error("The source file must be .PRN file.");
+}
 
 prn2xls.process(program.path);
