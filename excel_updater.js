bc93e676155168805671b4aa820cda77dea154fa
Chamnap Chhorn <chamnapchhorn@gmail.com>
2016-03-31 21:23:56 +0700

    use khmerfont

--- excel_updater.js
+++ excel_updater.js
@@ -3,6 +3,11 @@ var _ = require('lodash');
 var ExcelUpdater = function(workbook) {
   this.workbook = workbook;
   this.worksheet = this.workbook.getWorksheet(1);
+
+  this.rightAlignmentOptions = { alignment: { horizontal: 'right' } };
+  this.khmerFontOptions = { font: { name: 'Khmer OS', size: 9 } };
+
+  this.rightAlignmentKhmerFont = _.extend({}, this.rightAlignmentOptions, this.khmerFontOptions);
 };
 
 ExcelUpdater.prototype = {
@@ -23,11 +28,11 @@ ExcelUpdater.prototype = {
   },
 
   updateHeader: function(invoice, startingRow) {
-    this.setCell('B' + (1 + startingRow), 'វិក្កយប័ត្រអាករ');
+    this.setCell('B' + (1 + startingRow), 'វិក្កយប័ត្រអាករ', this.khmerFontOptions);
     this.setCell('B' + (2 + startingRow), 'VAT INVOICE');
-    this.setCell('B' + (3 + startingRow), 'លេខអត្តសញ្ញាណកម្ម អតប');
+    this.setCell('B' + (3 + startingRow), 'លេខអត្តសញ្ញាណកម្ម អតប', this.khmerFontOptions);
     this.setCell('B' + (4 + startingRow), 'VAT TIN: ' + invoice.tin);
-    this.setCell('B' + (6 + startingRow), 'ឈ្មោះអតិថិជន');
+    this.setCell('B' + (6 + startingRow), 'ឈ្មោះអតិថិជន', this.khmerFontOptions);
     this.setCell('C' + (6 + startingRow), invoice.customer.kh_name);
     this.setCell('B' + (7 + startingRow), 'Bill To:           ');
     this.setCell('C' + (7 + startingRow), invoice.customer.en_name);
@@ -36,52 +41,52 @@ ExcelUpdater.prototype = {
     this.setCell('C' + (10 + startingRow), invoice.customer.kh_address2);
     this.setCell('C' + (11 + startingRow), invoice.customer.en_address2);
 
-    this.setCell('G' + (3 + startingRow), 'ទំព័រ');
+    this.setCell('G' + (3 + startingRow), 'ទំព័រ', this.khmerFontOptions);
     this.setCell('G' + (4 + startingRow), 'Page No.:        ' + invoice.pageNumber);
-    this.setCell('G' + (6 + startingRow), 'លេខរៀងវិក្កយប័ត្រ');
+    this.setCell('G' + (6 + startingRow), 'លេខរៀងវិក្កយប័ត្រ', this.khmerFontOptions);
     this.setCell('G' + (7 + startingRow), 'Invoice  No.');
     this.setCell('H' + (7 + startingRow), ':');
     this.setCell('I' + (7 + startingRow), invoice.invoice.number);
 
-    this.setCell('G' + (8 + startingRow), 'កាលបរិចេ្ឆទ');
+    this.setCell('G' + (8 + startingRow), 'កាលបរិចេ្ឆទ', this.khmerFontOptions);
     this.setCell('G' + (9 + startingRow), 'Invoice  Dt.');
     this.setCell('H' + (9 + startingRow), ':');
     this.setCell('I' + (9 + startingRow), invoice.invoice.date);
 
-    this.setCell('G' + (10 + startingRow), 'លេខរៀងការបញ្ជាទិញ');
+    this.setCell('G' + (10 + startingRow), 'លេខរៀងការបញ្ជាទិញ', this.khmerFontOptions);
     this.setCell('G' + (11 + startingRow), 'S.O.No.     ');
     this.setCell('H' + (11 + startingRow), ':');
     this.setCell('I' + (11 + startingRow), invoice.soNumber);
 
-    this.setCell('B' + (13 + startingRow), 'លេខកូតអតិថិជន');
+    this.setCell('B' + (13 + startingRow), 'លេខកូតអតិថិជន', this.khmerFontOptions);
     this.setCell('B' + (14 + startingRow), 'C.Code :');
     this.setCell('C' + (14 + startingRow), invoice.cCode);
-    this.setCell('B' + (15 + startingRow), 'លេខអត្តសញ្ញាណកម្ម អតប');
+    this.setCell('B' + (15 + startingRow), 'លេខអត្តសញ្ញាណកម្ម អតប', this.khmerFontOptions);
     this.setCell('B' + (15 + startingRow), ' VAT    :');
 
-    this.setCell('G' + (13 + startingRow), 'កិច្ចសន្យាលេខ');
+    this.setCell('G' + (13 + startingRow), 'កិច្ចសន្យាលេខ', this.khmerFontOptions);
     this.setCell('G' + (14 + startingRow), 'Contract No.');
     this.setCell('H' + (14 + startingRow), ':');
     this.setCell('I' + (14 + startingRow), invoice.contract.number);
-    this.setCell('G' + (15 + startingRow), 'កាលបរិចេ្ឆទនៃកិច្ចសន្យា');
+    this.setCell('G' + (15 + startingRow), 'កាលបរិចេ្ឆទនៃកិច្ចសន្យា', this.khmerFontOptions);
     this.setCell('G' + (16 + startingRow), 'Contract Dt.');
     this.setCell('H' + (16 + startingRow), ':');
     this.setCell('I' + (16 + startingRow), invoice.contract.date);
 
-    this.setCell('B' + (17 + startingRow), 'ទំនាក់ទំនង');
+    this.setCell('B' + (17 + startingRow), 'ទំនាក់ទំនង', this.khmerFontOptions);
     this.setCell('B' + (18 + startingRow), 'Contact:');
     this.setCell('C' + (18 + startingRow), invoice.contact);
 
-    this.setCell('G' + (17 + startingRow), 'រយ:ពេលទូទាត់');
-    this.setCell('I' + (17 + startingRow), '៧ថ្ងៃបន្ទាប់ពីទទួលបានវិក្កយប័ត្រ');
+    this.setCell('G' + (17 + startingRow), 'រយ:ពេលទូទាត់', this.khmerFontOptions);
+    this.setCell('I' + (17 + startingRow), '៧ថ្ងៃបន្ទាប់ពីទទួលបានវិក្កយប័ត្រ', { alignment: { wrapText: true } });
     this.setCell('G' + (18 + startingRow), 'Terms:   7 days after the');
     this.setCell('G' + (19 + startingRow), 'date of receiving invoice');
 
-    this.setCell('B' + (22 + startingRow), 'លេខរៀង');
-    this.setCell('C' + (22 + startingRow), 'កូតទំនិញេ/សេវា');
-    this.setCell('D' + (22 + startingRow), 'បរិយាយមុខទំនិញ/សេវា');
-    this.setCell('G' + (22 + startingRow), 'បរិមាណ');
-    this.setCell('I' + (22 + startingRow), 'ចំនួនទឹកប្រាក់');
+    this.setCell('B' + (22 + startingRow), 'លេខរៀង', this.khmerFontOptions);
+    this.setCell('C' + (22 + startingRow), 'កូតទំនិញេ/សេវា', this.khmerFontOptions);
+    this.setCell('D' + (22 + startingRow), 'បរិយាយមុខទំនិញ/សេវា', this.khmerFontOptions);
+    this.setCell('G' + (22 + startingRow), 'បរិមាណ', this.khmerFontOptions);
+    this.setCell('I' + (22 + startingRow), 'ចំនួនទឹកប្រាក់', this.khmerFontOptions);
     this.setCell('B' + (23 + startingRow), 'Line');
     this.setCell('C' + (23 + startingRow), 'Item Code');
     this.setCell('D' + (23 + startingRow), 'Item Description');
@@ -138,28 +143,28 @@ ExcelUpdater.prototype = {
     if (invoice.subTotal) {
       var rowNumber = startingRow + 26 + 20 + 1;
       this.updateLineItemsFooter(rowNumber);
-      this.setCell('E' + (rowNumber + 1), 'សរុបទំព័រទី១​');
+      this.setCell('E' + (rowNumber + 1), 'សរុបទំព័រទី១​', this.khmerFontOptions);
       this.setCell('G' + (rowNumber + 1), 'Sub Total Page No.  1:  ');
-      this.setCell('I' + (rowNumber + 1), invoice.subTotal, this.rightAlignmentOptions());
+      this.setCell('I' + (rowNumber + 1), invoice.subTotal, this.rightAlignmentOptions);
 
       return rowNumber + 3;
     } else {
       var rowNumber = startingRow + 26 + 20;
-      this.setCell('F' + (rowNumber + 1), 'សរុប', this.rightAlignmentOptions());
-      this.setCell('G' + (rowNumber + 1), 'Total  :', this.rightAlignmentOptions());
-      this.setCell('I' + (rowNumber + 1), invoice.total, this.rightAlignmentOptions());
-      this.setCell('F' + (rowNumber + 2), 'អាករលើតម្លែបន្ថែម ១០%', this.rightAlignmentOptions());
-      this.setCell('G' + (rowNumber + 2), 'VAT 10%:', this.rightAlignmentOptions());
-      this.setCell('I' + (rowNumber + 2), invoice.vat, this.rightAlignmentOptions());
+      this.setCell('F' + (rowNumber + 1), 'សរុប', this.rightAlignmentKhmerFont);
+      this.setCell('G' + (rowNumber + 1), 'Total  :', this.rightAlignmentOptions);
+      this.setCell('I' + (rowNumber + 1), invoice.total, this.rightAlignmentOptions);
+      this.setCell('F' + (rowNumber + 2), 'អាករលើតម្លែបន្ថែម ១០%', this.rightAlignmentKhmerFont);
+      this.setCell('G' + (rowNumber + 2), 'VAT 10%:', this.rightAlignmentOptions);
+      this.setCell('I' + (rowNumber + 2), invoice.vat, this.rightAlignmentOptions);
 
       this.updateLineItemsFooter(rowNumber + 4);
-      this.setCell('F' + (rowNumber + 5), 'សរុបរួម', this.rightAlignmentOptions());
-      this.setCell('G' + (rowNumber + 5), 'GRAND TOTAL:', this.rightAlignmentOptions());
-      this.setCell('I' + (rowNumber + 5), invoice.grandTotal, this.rightAlignmentOptions());
+      this.setCell('F' + (rowNumber + 5), 'សរុបរួម', this.rightAlignmentKhmerFont);
+      this.setCell('G' + (rowNumber + 5), 'GRAND TOTAL:', this.rightAlignmentOptions);
+      this.setCell('I' + (rowNumber + 5), invoice.grandTotal, this.rightAlignmentOptions);
 
       // terms
-      this.setCell('B' + (rowNumber + 8), 'ការទូទាត់អាចធ្វើឡើងតាមមូលឡបទានប័ត្រ');
-      this.setCell('B' + (rowNumber + 9), 'ឬ តាមរយ:ការទូទាត់ទៅធនាគារ');
+      this.setCell('B' + (rowNumber + 8), 'ការទូទាត់អាចធ្វើឡើងតាមមូលឡបទានប័ត្រ', this.khmerFontOptions);
+      this.setCell('B' + (rowNumber + 9), 'ឬ តាមរយ:ការទូទាត់ទៅធនាគារ', this.khmerFontOptions);
       this.setCell('B' + (rowNumber + 10), 'Payment can be made by Cheque');
       this.setCell('B' + (rowNumber + 11), 'or Bank Transfer payable to :');
       this.setCell('B' + (rowNumber + 13), 'Account Name: ' + invoice.account.name);
@@ -167,7 +172,7 @@ ExcelUpdater.prototype = {
       this.setCell('B' + (rowNumber + 15), 'Bank Name   : ' + invoice.account.bank_name);
       this.setCell('B' + (rowNumber + 16), 'SWIFT CODE: ' + invoice.account.swift_code);
 
-      this.setCell('I' + (rowNumber + 8), 'ហត្ថលេខា');
+      this.setCell('I' + (rowNumber + 8), 'ហត្ថលេខា', this.khmerFontOptions);
       this.setCell('I' + (rowNumber + 9), 'Authorized Signature');
       this.setCell('I' + (rowNumber + 13), '-------------------------------');
 
@@ -196,8 +201,8 @@ ExcelUpdater.prototype = {
       self.setCell(lineCell, lineItem.line);
       self.setCell(codeItemCell, lineItem.codeItem);
       self.setCell(descriptionCell, lineItem.description);
-      self.setCell(spotCell, lineItem.spot, self.rightAlignmentOptions());
-      self.setCell(amountCell, lineItem.amount, self.rightAlignmentOptions());
+      self.setCell(spotCell, lineItem.spot, self.rightAlignmentOptions);
+      self.setCell(amountCell, lineItem.amount, self.rightAlignmentOptions);
 
       rowNumber++;
     });
@@ -214,10 +219,10 @@ ExcelUpdater.prototype = {
     if (options && options.border) {
       this.worksheet.getCell(cell).border = options.border;
     }
-  },
 
-  rightAlignmentOptions: function() {
-    return { alignment: { horizontal: 'right' } };
+    if (options && options.font) {
+      this.worksheet.getCell(cell).font = options.font;
+    }
   }
 };
 
