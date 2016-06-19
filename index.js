4f85bfa3559b2e32f1abe4b7be623cd5e7c4354a
radin-reth <radin.reth@gmail.com>
2016-06-19 13:11:28 +0700

    try pdf file not found

--- index.js
+++ index.js
@@ -93,10 +93,9 @@ var convertPdf = function(prnFile, destinationDirectory, options, callback) {
   var invoices  = prnParser.invoices;
   var invoice   = invoices[0];
 
-  var htmlFileName = 'pdf.html', 
-    pdfFileName    = 'page.pdf';
+  var htmlFileName = './pdf.html';
 
-  var child = exec('wkhtmltopdf -s A4 -L 15.05mm -R 19.05mm -T 19.05mm -B 19.05mm ' + htmlFileName + ' page.pdf', function(err, stdout, stderr) {
+  var child = exec('wkhtmltopdf -s A4 -L 15.05mm -R 19.05mm -T 19.05mm -B 19.05mm ' + htmlFileName + ' ./page.pdf', function(err, stdout, stderr) {
     if(err) { throw err; }
     util.log(stderr);
   });
